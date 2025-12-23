ce0e2fb7d85b880b2adfa1533fb4e76d67277c57
Cauanz <61758981+Cauanz@users.noreply.github.com>
2025-12-23 11:33:44 -0300

    ✨ Atualiza rotas de veículos para incluir métodos de criação e atualização; ajusta a serialização de dados na view index

--- backend/vehicles/urls.py
+++ backend/vehicles/urls.py
@@ -5,7 +5,8 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.index, name="index"),
-    path('create/', views.create, name="create")
+    path('create/', views.create, name="create"),
+    path('update/', views.update, name="update"),
 ]
 
 
--- backend/vehicles/views.py
+++ backend/vehicles/views.py
@@ -1,23 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from vehicles.models import Vehicle
 from django.views.decorators.csrf import csrf_exempt
 import json
+from django.core import serializers
 
 # TODO - TECNICAMENTE OS DOIS ESTÃO FUNCIONANDO, MAS CONTINUAR PARA CRIAR OBJETOS E RECUPERAR ELES DIREITO
 
-# Create your views here.
+#* GET ALL
 def index(request):
+  vehicles_json = serializers.serialize("json", Vehicle.objects.all())
   
-  vehicles = Vehicle.objects.all()
-  
-  return HttpResponse(vehicles)
+  return HttpResponse(vehicles_json)
 
 
 # TODO - LEMBRAR NO FRONT TEMOS QUE LIMITAR AS OPÇÕES DISPONIVEIS NOS CAMPOS COM CHOICE
 @csrf_exempt
 def create(request):
-  
+  # TODO - TALVEZ TAMBÉM ADICIONAR VERIFICAÇÃO DE AUTENTICAÇÃO MAIS TARDE 
   if request.method == 'POST':
     data = request.POST
     json_data = json.loads(request.body)
@@ -33,4 +33,33 @@ def create(request):
     )
     new_tank.save()
   
-  return HttpResponse('something')
+  redirect("/vehicles/")
+  return HttpResponse(content="Created successfully!", status=200)
+
+#* UPDATE BY ID
+def update(request):
+  
+  if request.method == 'POST':
+    json_data = json.loads(request.body)
+    
+    id = json_data.get("pk")
+    
+    if not id:
+      return HttpResponse(content="Error, ID not found", status=400)
+    
+    vehicle = Vehicle.objects.get("id")
+    print(vehicle)
+  
+  return HttpResponse(content="Done!")
+    # TODO - PAREI AQUI, NÃO SEI SE A ROTA FUNCIONA
+    
+#* GET BY ID
+
+
+
+
+#* DELETE ONE BY ID
+
+
+#* DELETE ALL
+
